41708cdcfc0f075b9959b0648eef9a7cc76d8d11
pimonwan.sutmee <pimonwan.sutmee@scb.co.th>
2019-07-02 17:48:25 +0700

    --

---
+++ kpi/kpi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kpi
+//
+//  Created by พิมลวรรณ สุดมี on 7/2/19.
+//  Copyright © 2019 พิมลวรรณ สุดมี. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
