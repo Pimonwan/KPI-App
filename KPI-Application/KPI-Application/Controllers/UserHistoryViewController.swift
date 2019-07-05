f72c1dfa1518bd88d2ceff49d201ceff5fec1278
Thammanoon Wethanyaporn <thammanoon.wethanyaporn@scb.co.th>
2019-07-05 16:34:30 +0700

    5 Friday 16:34

--- KPI-Application/KPI-Application/Controllers/UserHistoryViewController.swift
+++ KPI-Application/KPI-Application/Controllers/UserHistoryViewController.swift
@@ -2,10 +2,10 @@ import UIKit
 
 class UserHistoryViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
-
+    
     @IBOutlet weak var mTableView: UITableView!
     
-    var yearArr = ["2016","2017","2018"]
+    var yearArr = ["2019","2018","2017","2016","2015","2014","2013","2012"]
     var name = "Thammanoon Wethanyaporn"
     var id = "12345"
     
@@ -13,19 +13,21 @@ class UserHistoryViewController: UIViewController,UITableViewDataSource,UITableV
     var isCoolapce = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //        self.mName.text = "Thammanoon Wethanyaporn"
-        //        self.mID.text = "00000"
-        
-        mTableView.estimatedRowHeight = 301
-        mTableView.rowHeight = UITableView.automaticDimension
+        //        mTableView.estimatedRowHeight = 301
+        //        mTableView.rowHeight = UITableView.automaticDimension
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if self.SelectedIndex == indexPath.row && isCoolapce == true
-        {
-            return 301
-        }else{
-            return 50
+        let row = indexPath.row
+        if row == 0 {return 100}
+        if row == 1 {return 163}
+        else{
+            if self.SelectedIndex == indexPath.row && isCoolapce == true
+            {
+                return 335
+            }else{
+                return 50
+            }
         }
     }
     
@@ -34,14 +36,26 @@ class UserHistoryViewController: UIViewController,UITableViewDataSource,UITableV
     }
     
     
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "DropViewCell") as! DropTableViewCell
-        cell.mYear.text! = yearArr[indexPath.row]
-        //12.19
-        
-        return cell
+        let row = indexPath.row
+        if row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "UserDetailViewCell") as! UserDetailTableViewCell
+            return cell
+        }
+        if row == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ChartViewCell") as! ChartTableViewCell
+            return cell
+        }else{
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "DropViewCell") as! DropTableViewCell
+            cell.mYear.text! = yearArr[indexPath.row]
+            mTableView.rowHeight = UITableView.automaticDimension
+            //12.19
+            
+            return cell
+        }
     }
+    
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
@@ -59,5 +73,4 @@ class UserHistoryViewController: UIViewController,UITableViewDataSource,UITableV
         self.SelectedIndex = indexPath.row
         tableView.reloadRows(at: [indexPath], with: .automatic)
     }
-    
 }
